bffaadffe68c6f9febe1959240855e34d21dd8d1
TobiasAndren <tobiasandren@hotmail.com>
2025-09-16 12:14:11 +0200

    Added the ability to change color of eyes and body and the option to remove and add the hat

--- src/app/duck/page.tsx
+++ src/app/duck/page.tsx
@@ -1,9 +1,55 @@
+"use client";
+
 import DuckScene from "@/components/DuckScene";
+import { useState } from "react";
 
 export default function Duck() {
+  const [bodyColor, setBodyColor] = useState("#ffca4b");
+  const [eyeColor, setEyeColor] = useState("black");
+  const [isHatVisible, setIsHatVisible] = useState(true);
+
   return (
-    <div className="w-screen h-screen">
-      <DuckScene />
-    </div>
+    <main className="w-screen h-screen flex">
+      <div className="h-1/2 w-1/3">
+        <DuckScene
+          bodyColor={bodyColor}
+          eyeColor={eyeColor}
+          hatVisible={isHatVisible}
+        />
+        <button
+          className="bg-orange-300 h-3 w-7"
+          onClick={() => setBodyColor("#ffca4b")}
+        ></button>
+        <button
+          className="bg-red-500 h-3 w-7"
+          onClick={() => setBodyColor("red")}
+        ></button>
+        <button
+          className="bg-green-500 h-3 w-7"
+          onClick={() => setBodyColor("green")}
+        ></button>
+        <button
+          className="bg-black h-3 w-7"
+          onClick={() => setEyeColor("black")}
+        ></button>
+        <button
+          className="bg-blue-500 h-3 w-7"
+          onClick={() => setEyeColor("blue")}
+        ></button>
+        <button
+          className="bg-purple-500 h-3 w-7"
+          onClick={() => setEyeColor("purple")}
+        ></button>
+        {isHatVisible ? (
+          <button className="h-3 w-7" onClick={() => setIsHatVisible(false)}>
+            Remove Hat
+          </button>
+        ) : (
+          <button className="h-3 w-7" onClick={() => setIsHatVisible(true)}>
+            add hat
+          </button>
+        )}
+      </div>
+    </main>
   );
 }
