e71ba7fb6b8ff9ff4dffe1495b4d0960ce531dcf
Johan-Hagman <steven.gerard.hagman@gmail.com>
2025-09-17 14:50:00 +0200

    Restructure page.tsx, add gray bg in darkmode in layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,8 +33,6 @@ export default function RootLayout({
       >
         <Header />
         {children}
-        <ProductDescription />
-        <Gallery />
         <Footer />
       </body>
     </html>
