18fad9f86c857586839beee0a9f12c68c99f6585
Johan-Hagman <steven.gerard.hagman@gmail.com>
2025-09-25 18:07:44 +0200

    add className as prop for component to hero

--- src/components/layout/Gallery.tsx
+++ src/components/layout/Gallery.tsx
@@ -1,26 +1,32 @@
-export default function Gallery() {
+interface GalleryProps {
+  className?: string;
+}
+
+export default function Gallery({ className = "" }: GalleryProps) {
   return (
-    <div className="w-full px-14 py-24 flex justify-center items-center gap-5">
+    <div
+      className={`w-full px-14 py-24 flex justify-center items-center gap-5 ${className}`}
+    >
       <div className="flex justify-start items-center gap-5">
         <img
-          className=" h-[300px] rounded-[20px] object-cover"
+          className="h-[300px] xl:h-[500px] rounded-[20px] object-cover"
           src="/images/pic.png"
           alt="Yellow duck in bathtub"
         />
         <img
-          className="h-[300px] rounded-[20px] object-cover"
+          className="h-[300px] xl:h-[500px] rounded-[20px] object-cover"
           src="/images/groupDucks.png"
           alt="Ducklings swimming in pond"
         />
       </div>
       <div className="flex justify-start items-center gap-5">
         <img
-          className=" h-[300px] rounded-[20px] object-cover"
+          className="h-[300px] xl:h-[500px] rounded-[20px] object-cover"
           src="/images/bubbles.png"
           alt="Transparent bubbles floating"
         />
         <img
-          className="h-[300px] rounded-[20px] object-cover"
+          className="h-[300px] xl:h-[500px] rounded-[20px] object-cover"
           src="/images/ducks.png"
           alt="Pattern of yellow rubber ducks"
         />
--- src/components/layout/ProductDescription.tsx
+++ src/components/layout/ProductDescription.tsx
@@ -1,22 +1,29 @@
-export default function ProductDescription() {
+interface ProductDescriptionProps {
+  className?: string;
+}
+
+export default function ProductDescription({
+  className = "",
+}: ProductDescriptionProps) {
   return (
-    <div className="w-full pl-16 pr-14 pt-5 flex flex-col justify-center items-start gap-7">
+    <div
+      className={`w-full pl-16 pr-14 pt-5 flex flex-col justify-center items-start gap-7 ${className}`}
+    >
       <div className="flex flex-col justify-center items-start gap-2.5">
-        <div className="justify-start text-black text-xl font-bold font-['Inter']">
+        <div className="justify-start text-black text-xl font-bold">
           Product Details
         </div>
-        <div className="w-[632px] justify-start text-black text-xl font-normal font-['Inter']">
+        <div className="w-[632px] justify-start text-black text-xl font-normal">
           A classic favorite with a personal touch. This customizable rubber
           duck is simple, cheerful, and uniquely yours. A little companion
           designed to brighten any space or occasion
         </div>
       </div>
       <div className="flex flex-col justify-center items-start gap-2.5">
-        <div className="justify-start text-black text-xl font-bold font-['Inter']">
-          Size
-        </div>
-        <div className="w-[632px] justify-start text-neutral-400 text-xl font-normal font-['Inter']">
-          Dimensions: 8 cm × 7 cm × 9 cm Weight: approx. 45 g
+        <div className="justify-start text-black text-xl font-bold">Size</div>
+        <div className="w-[632px] justify-start text-neutral-400 text-xl font-normal">
+          Dimensions: 8 cm × 7 cm × 9 cm <br />
+          Weight: approx. 45 g
         </div>
       </div>
     </div>
