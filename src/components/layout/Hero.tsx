8b87a7d24cf90562db255bac5fc839facc01ba82
Johan-Hagman <steven.gerard.hagman@gmail.com>
2025-09-25 18:10:05 +0200

    structure right margins and basic colors when site renders

--- src/components/layout/Hero.tsx
+++ src/components/layout/Hero.tsx
@@ -5,13 +5,17 @@ import DuckScene from "../DuckScene";
 import DuckCustomizer from "./DuckCustomizer";
 import { eyeExpression } from "@/components/DuckModel";
 
-export default function Hero() {
+interface HeroProps {
+  className?: string;
+}
+
+export default function Hero({ className = "" }: HeroProps) {
   const [duckConfig, setDuckConfig] = useState({
     bodyColor: "#ffca4b",
     beakColor: "#f35626",
     hatVisible: true,
-    hatColor: "#a968c4",
-    badgeColor: "#5b1657",
+    hatColor: "#E572E3",
+    badgeColor: "#70C6FF",
     eyeExpression: "normal" as eyeExpression,
   });
 
@@ -20,8 +24,10 @@ export default function Hero() {
   };
 
   return (
-    <div className="w-full h-[958px] px-14 py-6 relative flex justify-start items-start gap-2.5">
-      <div className="w-1/2 h-full relative bg-blue-50 overflow-hidden">
+    <div
+      className={`w-full px-14 py-6 relative inline-flex justify-start items-center gap-2.5 overflow-hidden ${className}`}
+    >
+      <div className="w-[864px] h-[970px] relative bg-blue-50 overflow-hidden">
         <DuckScene
           bodyColor={duckConfig.bodyColor}
           hatVisible={duckConfig.hatVisible}
@@ -32,21 +38,23 @@ export default function Hero() {
         />
         <div className="absolute top-4 right-4 px-2.5 py-1 bg-neutral-50 rounded-[40px] flex justify-center items-center">
           <div className="pb-2.5 flex justify-start items-center gap-2.5">
-            <div className="w-6 h-6 relative">
-              <div className="w-3.5 h-3.5 left-[4px] top-[6px] absolute outline outline-[1.50px] outline-offset-[-0.75px] outline-Text-Grey"></div>
-              <div className="w-[3px] h-1.5 left-[17px] top-[3px] absolute outline outline-[1.50px] outline-offset-[-0.75px] outline-Text-Grey"></div>
-            </div>
+            <img
+              src="/icons/arrow-move-up-right.png"
+              alt="Move up right"
+              className="w-6 h-6"
+            />
           </div>
           <div className="flex justify-center items-center gap-2.5">
-            <div className="justify-start text-Text-Grey text-base font-bold font-['Inter']">
+            <div className="justify-start text-[#8E8E8E] text-base font-bold font-['Inter']">
               3D
             </div>
           </div>
           <div className="pt-2.5 flex justify-center items-end gap-2.5">
-            <div className="w-6 h-6 relative">
-              <div className="w-3.5 h-3.5 left-[5px] top-[3px] absolute outline outline-[1.50px] outline-offset-[-0.75px] outline-Text-Grey"></div>
-              <div className="w-[3px] h-1.5 left-[4px] top-[15px] absolute outline outline-[1.50px] outline-offset-[-0.75px] outline-Text-Grey"></div>
-            </div>
+            <img
+              src="/icons/arrow-move-up-left.png"
+              alt="Move up left"
+              className="w-6 h-6"
+            />
           </div>
         </div>
       </div>
